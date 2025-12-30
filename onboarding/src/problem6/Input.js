79003704946ff3d01c27d18062e1d4376e291d8d
lucy-kim05 <effelt0522@naver.com>
2025-12-30 16:52:38 +0900

    feat(Input): 입력 구현

---
+++ onboarding/src/problem6/Input.js
@@ -0,0 +1,30 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { MESSAGE } from './Constants.js';
+
+class Input {
+  async getNumber() {
+    const number = await MissionUtils.Console.readLineAsync(
+      MESSAGE.NUMBER_INPUT
+    );
+    return number;
+  }
+
+  async getInfo(number) {
+    let nameArr = [];
+    let emailArr = [];
+    MissionUtils.Console.print(
+      `크루의 정보를 이름, 이메일 형식으로 ${number}번 입력해주세요.`
+    );
+    for (let i = 0; i < number; i++) {
+      const input = await MissionUtils.Console.readLineAsync('');
+      const info = input.split(',').map((x) => x.trim());
+      const [a, b] = info;
+      nameArr.push(a);
+      emailArr.push(b);
+    }
+    MissionUtils.Console.print('입력이 완료되었습니다.');
+    return [nameArr, emailArr];
+  }
+}
+
+export default Input;
