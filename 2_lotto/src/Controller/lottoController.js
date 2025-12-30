6c84b6250309f908fe9f2c3e2af08fdda4f98192
stellalee1210 <yeobinlee1210@gmail.com>
2025-12-30 16:39:03 +0900

    refactor(lottoController): 함수 길이 조정

--- 2_lotto/src/Controller/lottoController.js
+++ 2_lotto/src/Controller/lottoController.js
@@ -6,21 +6,36 @@ import { LottoGame } from "../Model/LottoGame.js";
 import { winningNumParser } from "../Model/parser.js";
 export const lottoController = async () => {
   try {
-    //구입 금액 입력 및 검증
-    const purchaseInput = await getPurchase();
-    validator(TYPE.PURCHASE, purchaseInput);
-    const purchaseCount = Number(purchaseInput) / LOTTO_PRICE;
+    //로또 구입 개수
+    const purchaseCount = await getPurchase();
 
     //로또 게임 초기화
     const lottoGame = new LottoGame(purchaseCount);
     printLottery(purchaseCount, lottoGame.getLottoNumbers());
 
-    //당첨 번호 입력, 검증, 저장
-    const winningNumInput = await getWinningNum();
-    const parsedWinningNum = winningNumParser(winningNumInput);
-    validator(TYPE.WINNING, parsedWinningNum);
-    lottoGame.saveWinningNumbers(parsedWinningNum);
+    //당첨 금액 저장
+    getWinning(lottoGame);
   } catch (error) {
     printError(error.message);
   }
+};
+
+const getPurchase = async () => {
+  //구입 금액 입력 및 검증
+  const purchaseInput = await getPurchase();
+  validator(TYPE.PURCHASE, purchaseInput);
+  return Number(purchaseInput) / LOTTO_PRICE;
+};
+
+const getWinning = async (lottoGame) => {
+  //당첨 번호 입력, 검증, 저장
+  const winningNumInput = await getWinningNum();
+  const parsedWinningNum = winningNumParser(winningNumInput);
+  validator(TYPE.WINNING, parsedWinningNum);
+
+  saveWinning(lottoGame);
+};
+
+const saveWinning = (lottoGame) => {
+  lottoGame.saveWinningNumbers(parsedWinningNum);
 };
