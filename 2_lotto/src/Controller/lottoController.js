4ed753466cad2d113b88096efa69b01b938269e2
stellalee1210 <yeobinlee1210@gmail.com>
2025-12-30 15:59:13 +0900

    fix(lottoController): 로또 개수 구하는 로직 오류 수정

--- 2_lotto/src/Controller/lottoController.js
+++ 2_lotto/src/Controller/lottoController.js
@@ -1,5 +1,5 @@
 import { getPurchase } from "../View/input.js";
-import { printError } from "../View/output.js";
+import { printError, printLottery } from "../View/output.js";
 import { validator } from "../Model/validator.js";
 import { TYPE, LOTTO_PRICE } from "../constants.js";
 import { LottoGame } from "../Model/LottoGame.js";
@@ -8,7 +8,7 @@ export const lottoController = async () => {
     //구입 금액 입력 및 검증
     const purchaseInput = await getPurchase();
     validator(TYPE.PURCHASE, purchaseInput);
-    const purchaseCount = Number(purchaseInput) % LOTTO_PRICE;
+    const purchaseCount = Number(purchaseInput) / LOTTO_PRICE;
 
     //로또 게임 초기화
     const lottoGame = new LottoGame(purchaseCount);
