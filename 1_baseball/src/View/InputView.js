e48a53436ed73bbf9ae061349a09f61ef1304b3b
lucy-kim05 <effelt0522@naver.com>
2025-12-31 16:40:14 +0900

    feat(InputView): 숫자 입력 구현

--- 1_baseball/src/View/InputView.js
+++ 1_baseball/src/View/InputView.js
@@ -1,1 +1,9 @@
-class 
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../Constants/Message.js';
+
+class InputView {
+  readInput() {
+    const number = MissionUtils.Console.readAsyncLine(MESSAGE.READ_NUMBER);
+    return number;
+  }
+}
