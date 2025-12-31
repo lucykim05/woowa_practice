f33fe64cf150c255a9d4beda2b3f2be4907e7fe4
lucy-kim05 <effelt0522@naver.com>
2025-12-31 13:04:26 +0900

    refactor(Result): 결과 로직 분리

--- onboarding/src/problem7/App.js
+++ onboarding/src/problem7/App.js
@@ -1,71 +1,14 @@
-import Input from './Input.js';
-import Controller from './Controller.js';
-import Calculator from './Calculator.js';
 import Output from './Output.js';
+import Result from './Result.js';
 
 class App {
   async run() {
     try {
-      const scoreResult = await this.#calculate();
-      const sortedMap = this.#sort(scoreResult);
-      const result = this.#recommand(sortedMap);
+      const result = await Result.getResult();
       Output.printResult(result);
     } catch (error) {
       console.error(error);
     }
-  }
-
-  //input 받고 계산처리
-  async #calculate() {
-    try {
-      const input = new Input();
-      const name = await input.readUserName();
-      const friendArr = await input.readFriend();
-      const visitorArr = await input.readVisitor();
-      const scoreResult = this.#getScore(name, friendArr, visitorArr);
-      return scoreResult;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  // 점수 정렬
-  #sort(scoreResult) {
-    const sortedMap = new Map(
-      [...scoreResult.entries()].sort((a, b) => {
-        if (b[1] !== a[1]) return b[1] - a[1];
-        if (a[0] < b[0]) return -1;
-        if (a[0] > b[0]) return 1;
-        return 0;
-      })
-    );
-    return sortedMap;
-  }
-
-  // 점수 계산
-  #getScore(username, friendArr, visitorArr) {
-    const controller = new Controller(username, friendArr, visitorArr);
-    const friend = controller.getNewFriend(username, friendArr);
-    const scoreResult = Calculator.calculate(
-      friend,
-      visitorArr,
-      username,
-      controller
-    );
-    return scoreResult;
-  }
-
-  //추천인 정리
-  #recommand(sortedMap) {
-    const result = [];
-    [...sortedMap.entries()].map((x) => {
-      if (x[1] > 0) {
-        result.push(x[0]);
-      }
-    });
-
-    if (result.length > 5) return result.slice(0, 5);
-    return result;
   }
 }
 
