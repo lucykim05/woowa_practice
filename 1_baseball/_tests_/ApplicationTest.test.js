930dbf3ec9be9455bea2e1339a3a2be2a9d55d66
stellalee1210 <yeobinlee1210@gmail.com>
2025-12-31 14:51:49 +0900

    chore: 환경설정

---
+++ 1_baseball/_tests_/ApplicationTest.test.js
@@ -0,0 +1,62 @@
+import App from "../src/App.js";
+import { Console, Random } from "@woowacourse/mission-utils";
+
+const mockQuestions = (answers) => {
+  Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("숫자 야구 게임", () => {
+  test("게임 종료 후 재시작", () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.run();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("예외 테스트", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["1234"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.run();
+    }).toThrow();
+  });
+});
