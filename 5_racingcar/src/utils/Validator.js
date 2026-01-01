9255efbf6d70ec5b49e4f649cc0850e0facf1a8c
lucy-kim05 <effelt0522@naver.com>
2026-01-01 15:55:15 +0900

    feat(Validator): 숫자 검증 구현

--- 5_racingcar/src/utils/Validator.js
+++ 5_racingcar/src/utils/Validator.js
@@ -6,6 +6,11 @@ class Validator {
     this.#checkLength(input);
   }
 
+  static validateNum(input) {
+    this.#checkIsNumber(input);
+    this.#checkIsInteger(input);
+  }
+
   static #checkEnglish(input) {
     const regex = /^[a-zA-Z]+$/;
     if (!regex.test(input)) throw new Error(ERROR.NOT_ENGLISH);
@@ -14,6 +19,14 @@ class Validator {
   static #checkLength(input) {
     if (input.length < 1 || input.length > 5)
       throw new Error(ERROR.WRONG_LENGTH);
+  }
+
+  static #checkIsNumber(input) {
+    if (!Number.isNaN(input)) throw new Error(ERROR.NOT_A_NUMBER);
+  }
+
+  static #checkIsInteger(input) {
+    if (!Number.isInteger(input)) throw new Error(ERROR.NOT_INTEGER);
   }
 }
 
