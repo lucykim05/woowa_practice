f0996ed7d351faac6c51f0cfe82fa9dd0219de43
lucy-kim05 <effelt0522@naver.com>
2026-01-01 16:41:35 +0900

    feat(Car): 차 객체 구현

--- 5_racingcar/src/Model/Car.js
+++ 5_racingcar/src/Model/Car.js
@@ -1,3 +1,12 @@
-class Car {}
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move(input) {
+    if (input >= 4) this.position++;
+  }
+}
 
 export default Car;
