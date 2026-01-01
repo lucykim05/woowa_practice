73b06dbe7ea0965bd7dc36265c84f7d7771b61c6
lucy-kim05 <effelt0522@naver.com>
2026-01-01 16:53:20 +0900

    feat(Car): 차 객체 내부에 상태 변경 로직 구현

--- 5_racingcar/src/Model/Car.js
+++ 5_racingcar/src/Model/Car.js
@@ -1,11 +1,19 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class Car {
   constructor(name) {
     this.name = name;
     this.position = 0;
   }
 
-  move(input) {
-    if (input >= 4) this.position++;
+  #move() {
+    const num = this.#getRandom();
+    if (num >= 4) this.position++;
+  }
+
+  #getRandom() {
+    const num = MissionUtils.Random.pickNumberInRange(0, 9);
+    return num;
   }
 }
 
