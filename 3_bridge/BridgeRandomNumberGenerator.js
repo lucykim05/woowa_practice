bcb862afd379b1cf2d65782eb8222a81ba355024
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-01 16:29:23 +0900

    chore: 개발환경 세팅

---
+++ 3_bridge/BridgeRandomNumberGenerator.js
@@ -0,0 +1,15 @@
+import { Random } from "@woowacourse/mission-utils";
+
+const BridgeRandomNumberGenerator = {
+  RANDOM_LOWER_INCLUSIVE: 0,
+  RANDOM_UPPER_INCLUSIVE: 1,
+
+  generate() {
+    return Random.pickNumberInRange(
+      this.RANDOM_LOWER_INCLUSIVE,
+      this.RANDOM_UPPER_INCLUSIVE
+    );
+  },
+};
+
+export default BridgeRandomNumberGenerator;
