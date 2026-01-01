7f1aadb2d31dbf71029d47a4a2f1692c432d0e4f
lucy-kim05 <effelt0522@naver.com>
2026-01-01 16:48:09 +0900

    feat(GameRound): 게임 라운드 기본 구현

--- 5_racingcar/src/Controller/GameRound.js
+++ 5_racingcar/src/Controller/GameRound.js
@@ -1,3 +1,16 @@
-class GameRound {}
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class GameRound {
+  #number;
+  constructor(cars) {
+    this.#pickRandom();
+    this.cars = cars;
+  }
+
+  #pickRandom() {
+    const num = MissionUtils.Random.pickNumberInRange(0, 9);
+    this.number = num;
+  }
+}
 
 export default GameRound;
