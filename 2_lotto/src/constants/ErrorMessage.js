95e171ebcb26f1ea9977fbd8433add53339f3030
lucy-kim05 <effelt0522@naver.com>
2026-01-02 15:44:07 +0900

    feat(NumberValidator): 숫자 검증 구현

---
+++ 2_lotto/src/constants/ErrorMessage.js
@@ -0,0 +1,4 @@
+export const ERROR = Object.freeze({
+  NOT_A_NUMBER: '[ERROR] 숫자를 입력해주세요.',
+  NOT_NATURAL: '[ERROR] 자연수를 입력해주세요.',
+});
--- 2_lotto/src/controller/GameController.js
+++ 2_lotto/src/controller/GameController.js
@@ -3,10 +3,14 @@ import InputView from '../view/InputView.js';
 class GameController {
   async run() {
     const input = new InputView();
-    const amount = await input.readAmount();
+    await this.getAmount(input);
     const winningNumbers = await input.readWinningNumbers();
     const bonusNumber = await input.readBonusNumber();
     return [amount, winningNumbers, bonusNumber];
+  }
+
+  async getAmount(input) {
+    const amount = await input.readAmount();
   }
 }
 
