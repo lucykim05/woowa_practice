1a51f861529c849f9a8b2cb64cea8bea26c8b64f
lucy-kim05 <effelt0522@naver.com>
2026-01-02 20:57:05 +0900

    feat(InputView): 입력 구현

--- 3_bridge/src/InputView.js
+++ 3_bridge/src/InputView.js
@@ -1,3 +1,6 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { MESSAGE } from './Constants.js';
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +8,26 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  async readBridgeSize() {
+    const length = await MissionUtils.Console.readLineAsync(MESSAGE.LENGTH);
+    return length;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  async readMoving() {
+    const move = await MissionUtils.Console.readLineAsync(MESSAGE.MOVE);
+    return move;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  async readGameCommand() {
+    const retry = await MissionUtils.Console.readLineAsync(MESSAGE.RETRY);
+    return retry;
+  },
 };
 
 export default InputView;
