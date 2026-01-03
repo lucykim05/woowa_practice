8cad3b7abb2e1f19ef2ae049dffe343cccc48dbd
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-03 17:04:20 +0900

    fix(Validator): 검증 기준 오류 수정

--- 3_bridge/src/Model/Validator.js
+++ 3_bridge/src/Model/Validator.js
@@ -2,9 +2,9 @@ import { ERROR, BRIDGE_SIZE } from "../constants.js";
 
 export const Validator = {
   bridgeLength(value) {
-    if (value.length() === 0) throw Error(ERROR.EMPTY);
+    if (value.length === 0) throw Error(ERROR.EMPTY);
     const number = Number(value);
-    if (Number.isInteger(number)) throw Error(ERROR.NAN);
+    if (!Number.isInteger(number)) throw Error(ERROR.NAN);
     if (number < BRIDGE_SIZE.MIN || number > BRIDGE_SIZE.MAX)
       throw Error(ERROR.OUT_OF_RANGE);
   },
