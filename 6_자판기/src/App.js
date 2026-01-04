4e31bd68a92e33c482e7a016ef63634341aa074c
lucy-kim05 <effelt0522@naver.com>
2026-01-04 15:27:42 +0900

    fix: 테스트 통과

--- 6_자판기/src/App.js
+++ 6_자판기/src/App.js
@@ -1,13 +1,18 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import MachineController from './controller/MachineController.js';
 
 class App {
   async play() {
-    const controller = new MachineController();
-    await controller.makeCoin();
-    await controller.makeGoods();
-    await controller.readUserAmount();
-    await controller.makePurchase();
-    await controller.printResult();
+    try {
+      const controller = new MachineController();
+      await controller.makeCoin();
+      await controller.makeGoods();
+      await controller.readUserAmount();
+      await controller.makePurchase();
+      await controller.printResult();
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+    }
   }
 }
 
