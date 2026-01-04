e95f600b4250db96be4e1bf6544dec37966b7bbf
lucy-kim05 <effelt0522@naver.com>
2026-01-04 17:07:51 +0900

    feat(Store): 프로모션 적용

--- 11_convenience_store/src/controller/Controller.js
+++ 11_convenience_store/src/controller/Controller.js
@@ -1,4 +1,5 @@
 import InputView from '../view/InputView.js';
+import Store from '../model/Store.js';
 
 class Controller {
   #input;
@@ -11,7 +12,7 @@ class Controller {
     this.#input = new InputView();
     const productsData = this.#input.readGoodsInfo();
     const promotionData = this.#input.readPromoInfo();
-    this.#store = new this.#store(productsData, promotionData);
+    this.#store = new Store(productsData, promotionData);
   }
 }
 
---
+++ 11_convenience_store/src/model/Store.js
@@ -0,0 +1,24 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class Store {
+  #products;
+
+  constructor(products, promotion) {
+    this.#products = products;
+    this.#checkPromotion(promotion);
+  }
+
+  #checkPromotion(promotion) {
+    const today = MissionUtils.DateTimes.now();
+    const promo = [];
+    promotion.forEach((x) => {
+      if (new Date(x.start_date) <= today && new Date(x.end_date) >= today)
+        promo.push(x.name);
+    });
+    this.#products.forEach((x) => {
+      if (!promo.includes(x.promotion)) x.promotion = null;
+    });
+  }
+}
+
+export default Store;
