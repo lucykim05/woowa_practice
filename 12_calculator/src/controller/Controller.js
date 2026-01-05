83667f83c5d202459211e610bfb37b354fd8ea2c
lucy-kim05 <effelt0522@naver.com>
2026-01-05 19:08:12 +0900

    feat(Controller): 입력 받아옴

--- 12_calculator/src/controller/Controller.js
+++ 12_calculator/src/controller/Controller.js
@@ -1,3 +1,10 @@
-class Controller {}
+import InputView from '../view/InputView.js';
+
+class Controller {
+  async readInput() {
+    const result = await InputView.readInput();
+    return result;
+  }
+}
 
 export default Controller;
