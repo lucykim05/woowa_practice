80637717bfee6c85331869b532039d606a4f97fd
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-05 10:44:24 +0900

    innitial commit

--- 4_menu/src/Utils/parser.js
+++
@@ -1,9 +0,0 @@
-export const parseWithComma = (value) => {
-  return value.split(",").map((v) => v.trim());
-};
-
-export const getAll2DimArrayValues = (array) => {
-  const allValues = [];
-  array.map((row) => allValues.push(...row));
-  return allValues;
-};
