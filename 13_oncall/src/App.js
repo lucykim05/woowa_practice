ed6671fcba359931fd9022e37d5c13b7dbfe12e6
lucy-kim05 <effelt0522@naver.com>
2026-01-07 13:05:49 +0900

    docs: 문서 작성

--- 13_oncall/src/App.js
+++ 13_oncall/src/App.js
@@ -1,18 +1,5 @@
-import InputView from './InputView.js';
-import EmergencyScheduleGenerator from './EmergencyScheduleGenerator.js';
-import OutputView from './OutputView.js';
-
 class App {
-  async run() {
-    const monthAndDayOfWeek = await InputView.getMonthAndDayOfWeek();
-    const month = Number(monthAndDayOfWeek[0]);
-    const dayOfWeek = monthAndDayOfWeek[1];
-    const workSchedule = await InputView.getEmergencyWorkSchedule();
-    const weekDaySchedule = workSchedule[0];
-    const offDaySchedule = workSchedule[1];
-    const totalSchedule = EmergencyScheduleGenerator.generateEmergencySchedule(month, dayOfWeek, weekDaySchedule, offDaySchedule);
-    OutputView.printSchedule(month, totalSchedule);
-  }
+  async run() {}
 }
 
 export default App;
