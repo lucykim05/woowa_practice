7b5c91a595de497f12d108e11f1b9f2da1715be8
lucy-kim05 <effelt0522@naver.com>
2026-01-07 13:22:37 +0900

    feat(Controller): calendar 생성 로직 구현

--- 13_oncall/src/controller/Controller.js
+++ 13_oncall/src/controller/Controller.js
@@ -1,5 +1,6 @@
 import Validator from '../model/Validator.js';
 import InputView from '../view/InputView.js';
+import Calendar from '../model/Calendar.js';
 
 class Controller {
   #calendar;
@@ -9,6 +10,9 @@ class Controller {
   async makeCalendar() {
     const date = await InputView.readDate();
     Validator.validateDate();
+    const calendar = new Calendar();
+    this.#calendar = calendar;
+    calendar.initCalendar(date);
   }
 }
 
