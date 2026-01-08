be2e88c58d547b2bb212aab5c01716ba0f042e07
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-08 14:04:17 +0900

    chore: 기초 토대 생성

---
+++ 14_attendance/src/View/OutputView.js
@@ -0,0 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+import { DEFAULT } from "../constants.js";
+export const OutputView = {
+  start() {
+    Console.print(DEFAULT.START);
+  },
+  progress() {},
+  result() {},
+};
---
+++ 14_attendance/src/constants.js
@@ -0,0 +1,7 @@
+export const DEFAULT = {
+  START: "",
+};
+
+export const ERROR = {
+  EMPTY: "[ERROR] 값이 비었습니다. 다시 입력해주세요.",
+};
