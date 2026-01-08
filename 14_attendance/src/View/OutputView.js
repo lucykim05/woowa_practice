77da9e2614eac1ad80afb29e68e42471ae5c33a5
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-08 16:06:59 +0900

    fix(OutputView): 시작메세지 형식 수정

--- 14_attendance/src/View/OutputView.js
+++ 14_attendance/src/View/OutputView.js
@@ -1,9 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
-import { DEFAULT } from "../constants.js";
 export const OutputView = {
   start(msg) {
     Console.print(msg);
-    Console.print(DEFAULT.START);
   },
   progress() {},
   result() {},
