6eb60766b5d5245caddc1dc99d89ac43cdaa2790
lucy-kim05 <effelt0522@naver.com>
2026-01-09 11:28:38 +0900

    feat(Validator): 사용자 입력 및 검증 후 return

--- 5_레이싱/src/Controller/GameController.js
+++ 5_레이싱/src/Controller/GameController.js
@@ -1,3 +1,22 @@
+import Validator from '../model/Validator.js';
+import InputView from '../view/InputView.js';
+
 class GameController {
-  async readInfo() {}
+  async readInfo() {
+    const names = await this.readNames();
+    const count = await this.readCount();
+    const game = new Game(names);
+  }
+
+  async readNames() {
+    const names = InputView.readNames();
+    Validator.validateNames(names);
+    return names;
+  }
+
+  async readCount() {
+    const count = InputView.readCount();
+    Validator.validateCount(count);
+    return count;
+  }
 }
