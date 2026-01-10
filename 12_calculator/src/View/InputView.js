e0a1acd2c5801f6139dbad11d0850bda6ba9f0c0
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-10 09:25:04 +0900

    파일 옮기기

--- 12_calculator/src/View/InputView.js
+++ 12_calculator/src/View/InputView.js
@@ -1,6 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
+import { DEFAULT } from "../constants.js";
 export const InputView = {
   async option() {
-    return await Console.readLineAsync();
+    return await Console.readLineAsync(DEFAULT.START);
   },
 };
